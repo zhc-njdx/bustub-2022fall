633a8ad6d3bf5cbeca4ac0d1cb0896d86c98f67a
zhc-njdx <2731293166@qq.com>
2023-05-12 20:40:39 +0800

    make clean

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -14,7 +14,9 @@
 
 namespace bustub {
 
-LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
+LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {
+    
+}
 
 auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
 
